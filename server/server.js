18dcfa49ba387957cc4fa6fac736052f0f8f17dc
naquoc17 <naquoc17.konto@gmail.com>
2024-08-20 15:04:33 +0200

    Test schema graphQL

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware as apolloMiddleware } from "@apollo/server/express4";
 import cors from "cors";
 import express from "express";
-import { authMiddleware, handleLogin } from "./auth";
+import { readFile } from "node:fs/promises";
+import { authMiddleware, handleLogin } from "./auth.js";
+import { resolvers } from "./resolvers.js";
 
 const PORT = 9000;
 
@@ -11,12 +13,15 @@ app.use(cors(), express.json(), authMiddleware);
 
 app.post("/login", handleLogin);
 
+const typeDefs = await readFile("./schema.graphql", "utf8");
 const apolloServer = new ApolloServer({
-   /* typeDefs, resolvers */
+   typeDefs,
+   resolvers,
 });
 await apolloServer.start();
 app.use("/graphql", apolloMiddleware(apolloServer));
 
 app.listen(PORT, () => {
    console.log(`Server is running on port ${PORT}`);
+   console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
 });
