b462ecaf5bfc5e4206b8c04de5b111acf8680808
naquoc17 <naquoc17.konto@gmail.com>
2024-08-27 22:18:01 +0200

    Error message for Loading data Updated

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -6,12 +6,36 @@ import JobList from "../components/JobList";
 
 export default function CompanyPage() {
    const { companyId } = useParams();
-   const [company, setCompany] = useState();
+   const [state, setState] = useState({
+      company: null,
+      loading: true,
+      error: false,
+   });
+
    useEffect(() => {
-      getCompany(companyId).then(setCompany);
+      (async () => {
+         try {
+            const company = await getCompany(companyId);
+            setState({ company, loading: false, error: false });
+         } catch (error) {
+            console.log("Error: ", JSON.stringify(error, null, 2));
+            setState({ company: null, loading: false, error: true });
+         }
+      })();
    }, [companyId]);
 
-   if (!company) return <p>Loading...</p>;
+   console.log("[CompanyPage] state: ", state);
+
+   const { company, loading, error } = state;
+
+   if (loading) {
+      return <div>Loading...</div>;
+   }
+
+   if (error) {
+      return <div className="has-text-danger">Data unavailable</div>;
+   }
+
    return (
       <div>
          <h1 className="title">{company.name}</h1>
